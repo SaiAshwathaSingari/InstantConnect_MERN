c2cb99c38fcca1ab67e05d0ad6f1e8139d27404b
SaiAshwathaSingari <singarisai777@gamil.com>
2025-09-28 23:34:51 +0530

    Backend Dev

--- server/server.js
+++ server/server.js
@@ -4,12 +4,33 @@ import http from 'http';
 import { connectDB } from './lib/db_connect.js';
 import userRouter from './routes/userRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import { Socket } from 'dgram';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
-
+export const io = new Server(server,{
+  cors:{
+    origin: "*"
+  }
+});
+//store users online
+export const userSocketMap = {};
+io.on("connection",(Socket)=>{
+  const userId = Socket.handshake.query.userId;
+  console.log("User connected", userId);
+  if(userId) userSocketMap[userId] = Socket.id;
+  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  Socket.on("disconnect",()=>{
+    console.log("User disconnected", userId);
+    if(userId) delete userSocketMap[userId];
+    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  } 
+)}); 
 //middleware Setup
 app.use(express.json());
 app.use(cors());
